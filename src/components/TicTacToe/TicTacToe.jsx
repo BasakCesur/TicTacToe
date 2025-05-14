3e46d8f461229efda868cf5b350ebcbc84232f41
Başak Özenalp Cesur <basak.ozenalp@gmail.com>
2025-05-14 23:02:58 +0300

    Fixed the win condition check in the TicTacToe component and added a draw message

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -52,19 +52,21 @@ const TicTacToe = () => {
       won(data[8]);
     } else if (data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
       won(data[8]);
-    } else if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
-      won(data[2]);
     } else if (data[2] === data[4] && data[4] === data[6] && data[6] !== "") {
       won(data[6]);
+    }
+    if (data.every((cell) => cell !== "") && !lock) {
+      titleRef.current.innerHTML = `It's a <span style="color:#ff0">Draw!</span>`;
+      setLock(true);
     }
   };
 
   const won = (winner) => {
     setLock(true);
     if (winner === "X") {
-      titleRef.current.innerHTML = `Conguratulation: <img src=${cross_icon} /> You Won`;
+      titleRef.current.innerHTML = `Congratulations: <img src=${cross_icon} /> You Won`;
     } else {
-      titleRef.current.innerHTML = `Conguratulation: <img src=${circle_icon} /> You Won`;
+      titleRef.current.innerHTML = `Congratulations: <img src=${circle_icon} /> You Won`;
     }
   };
 
